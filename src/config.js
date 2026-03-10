49a2dd5e7b5956a4e126bd008bf9ead1fbc8065a
dhangar007A <147794851+dhangar007A@users.noreply.github.com>
2026-03-10 12:05:29 +0530

    Add Azure deployment configuration

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export const API_KEY = "wx88eihaxkz1vdd9p"; 
-export const BASE_URL = "https://techhk.aoscdn.com";
+export const API_KEY = import.meta.env.VITE_API_KEY || "wx88eihaxkz1vdd9p"; 
+export const BASE_URL = import.meta.env.VITE_BASE_URL || "https://techhk.aoscdn.com";
